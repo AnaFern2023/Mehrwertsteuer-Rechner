eec61b86378f9094856b51d2bab966c96113c2aa
AnaFern2023 <AnaFern2023@gmail.com>
2023-03-13 11:23:19 +0100

    second_step

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 const nettoBrutto = document.getElementById("nettoBrutto");
-// const bruttoNetto = document.getElementById("bruttoNetto");
+const bruttoNetto = document.getElementById("bruttoNetto");
 const nineteen = document.getElementById("nineteen");
 const seven = document.getElementById("seven");
 const inputNumber = document.getElementById("inputNumber");
@@ -8,35 +8,55 @@ const resultMwst = document.getElementById('resultMwst');
 const endBetrag = document.getElementById('endBetrag');
 const textMwst = document.getElementById('textMwst');
 
-let berechnen = () => {
+const berechnen = () => {
 
     if (nettoBrutto.checked == true && nineteen.checked == true) {
-        // console.log(inputNumber.value * 1.19 + "€").toFixed(2);
         result.innerHTML = (inputNumber.value * 1.19).toFixed(2).concat("€");
         resultMwst.innerHTML = (inputNumber.value * 0.19).toFixed(2).concat("€");
-        endBetrag.innerHTML = ("Bruttobetrag (Endpreis)");
     }
     else if (nettoBrutto.checked == false && nineteen.checked == true) {
         result.innerHTML = (inputNumber.value / 1.19).toFixed(2).concat("€");
         resultMwst.innerHTML = (inputNumber.value / 1.19 * 0.19).toFixed(2).concat("€");
-        endBetrag.innerHTML = ("Nettobetrag");
     }
     else if (nettoBrutto.checked == true && seven.checked == true) {
         result.innerHTML = (inputNumber.value * 1.07).toFixed(2).concat("€");
         resultMwst.innerHTML = (inputNumber.value * 0.07).toFixed(2).concat("€");
-        endBetrag.innerHTML = ("Bruttobetrag (Endpreis)");
     }
     else if (nettoBrutto.checked == false && seven.checked == true) {
         result.innerHTML = (inputNumber.value / 1.07).toFixed(2).concat("€");
         resultMwst.innerHTML = (inputNumber.value / 1.07 * 0.07).toFixed(2).concat("€");
-        endBetrag.innerHTML = ("Nettobetrag");
     }
 }
 
-let text = () => {
-    if (nettoBrutto.checked == true) {
+
+// const berechnen = () => {
+//     if (nettoBrutto.checked) {
+//         if (nineteen.checked) {
+//             result.innerHTML = (inputNumber.value * 1.19).toFixed(2).concat("€");
+//             resultMwst.innerHTML = (inputNumber.value * 0.19).toFixed(2).concat("€");
+//         } else {
+//             result.innerHTML = (inputNumber.value * 1.07).toFixed(2).concat("€");
+//             resultMwst.innerHTML = (inputNumber.value * 0.07).toFixed(2).concat("€");
+//         }
+//     }
+//     else {
+//         if (bruttoNetto.checked) {
+//             result.innerHTML = (inputNumber.value / 1.19).toFixed(2).concat("€");
+//             resultMwst.innerHTML = (inputNumber.value / 1.19 * 0.19).toFixed(2).concat("€");
+//         } else {
+//             result.innerHTML = (inputNumber.value / 1.07).toFixed(2).concat("€");
+//             resultMwst.innerHTML = (inputNumber.value / 1.07 * 0.07).toFixed(2).concat("€");
+//         }
+//     }
+// }
+
+
+const text = () => {
+    if (nettoBrutto.checked) {
         textMwst.innerHTML = ("Nettobetrag (Preis ohne Mehrwertsteuer) in Euro");
+        endBetrag.innerHTML = ("Bruttobetrag (Endpreis)");
     } else {
         textMwst.innerHTML = ("Bruttobetrag (Preis inklusive Mehrwertsteuer) in Euro");
+        endBetrag.innerHTML = ("Nettobetrag");
     }
 }
